713cfe0568a977ec715ff12ee9735fabdcd88b67
Quentin Maucher <quentinmaucher@icloud.com>
2026-01-29 12:45:46 -0500

    video-record button

--- JavaScript/oral-exam_frontend_main_student.js
+++ JavaScript/oral-exam_frontend_main_student.js
@@ -1,11 +1,18 @@
-document.querySelector('#start').addEventlistener('click', aufnahmestarten());
+//document.querySelector('#start').addEventlistener('click', aufnahmestarten());
 
-function aufnahmestarten ()
-{
-navigator.mediadevices.getusermedia({
-    video: true,
-    audio: true
+//function aufnahmestarten ()
+//{
+//navigator.mediadevices.getusermedia({
+  //  video: true,
+   // audio: true
 
-}
-)
-}
+//}
+//)
+//}
+
+document.querySelector('#start').addEventListener('click', async (e) => {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: true
+  })
+  document.querySelector('video').srcObject = stream
+})
