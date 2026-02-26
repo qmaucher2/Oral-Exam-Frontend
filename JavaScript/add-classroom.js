32b0d12a75cf379de5eca9a50d84d3b03cdd3b90
Quentin Maucher <quentinmaucher@icloud.com>
2026-02-26 13:41:29 -0500

    CSS - Background and formatting

--- JavaScript/add-classroom.js
+++ JavaScript/add-classroom.js
@@ -4,8 +4,7 @@ function addClassroom() {
     const insertfield = document.getElementById("created-classrooms")
     const newClassroom = document.createElement("div");
     newClassroom.innerHTML = `
-        <li><a href="Template-Classroom${placeholder}_teacher-pov.html">Template Classroom-${placeholder}</a></li>
-        <br>
+        <li class="classroom-bullet-point"><a href="Template-Classroom${placeholder}_teacher-pov.html">Template Classroom-${placeholder}</a></li>
         `;
     insertfield.appendChild(newClassroom);
     placeholder++;
